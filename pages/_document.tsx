3ee7239e5ab2757590fb17fef31252d4ac28cd86
Jeve-Stobs <samuelacasey@gmail.com>
2022-01-22 15:58:05 -0500

    add preconnection

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,12 @@ class MyDocument extends Document {
           <meta name="twitter:creator" className="@devstobs" />
           <meta name="twitter:title" className="Jeve Stobs" />
           <meta name="twitter:description" className="Jeve Stobs - Portfolio" />
+          <link rel="preconnect" href="https://acoustic.to" />
+          <link rel="preconnect" href="https://github.com" />
+          <link rel="preconnect" href="https://twitter.com" />
+          <link rel="dns-prefetch" href="https://acoustic.to" />
+          <link rel="dns-prefetch" href="https://github.com" />
+          <link rel="dns-prefetch" href="https://twitter.com" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600.700&display=swap"
