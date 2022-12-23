fa98069c9db86feff9e8ce1ac80a45a12d48945e
Jeve Stobs <sam@jevestobs.dev>
2022-12-22 23:04:21 -0500

    ⬆️

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
   swcMinify: true,
   experimental: {
     optimizeCss: true,
-    enableUndici: true,
     fontLoaders: [
       { loader: '@next/font/google', options: { subsets: ['latin'] } }
     ]
